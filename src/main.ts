0419b3d40d058a76f7cf02eb35a3b4e643fdf3d2
Fenes Áron <fenes.aron@encosoft.hu>
2022-01-27 13:08:15 +0100

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.use(morgan('tiny'));
   app.getHttpAdapter().getInstance().set('etag', false);
   await app.listen(process.env.PORT || 3000);
 }
